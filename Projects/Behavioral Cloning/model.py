113b212fbeb037ca171ee6081b9e23557553d600
SWalls <soerenwalls@gmail.com>
2017-04-19 19:36:56 -0600

    Added new training data and updated model to nvidia.

--- Projects/Behavioral Cloning/model.py
+++ Projects/Behavioral Cloning/model.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
+from keras.layers import Cropping2D
 from keras.layers.core import Lambda, Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
@@ -12,13 +13,23 @@ with open('data/driving_log.csv') as csvfile:
     for line in reader:
         lines.append(line)
 
+newdata_cutoff = len(lines)
+
+with open('newdata/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
 images = []
 measurements = []
 for i in range(1, len(lines)):
     line = lines[i]
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = 'data/IMG/' + filename
+    folder = 'data'
+    if i >= newdata_cutoff:
+        folder = 'newdata'
+    current_path = folder + '/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -36,17 +47,25 @@ y_train = np.array(augmented_measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(16, 16, 3))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24, 5, 5))
 model.add(MaxPooling2D((2, 2)))
 model.add(Activation('relu'))
-model.add(Convolution2D(6, 6, 3))
+model.add(Convolution2D(36, 5, 5))
 model.add(MaxPooling2D((2, 2)))
+model.add(Activation('relu'))
+model.add(Convolution2D(48, 5, 5))
+model.add(MaxPooling2D((2, 2)))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3))
 model.add(Dropout(0.5))
 model.add(Activation('relu'))
 model.add(Flatten())
-model.add(Dense(84))
-model.add(Dense(28))
-model.add(Dense(1))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
