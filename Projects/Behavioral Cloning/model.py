bb427419e3576eb954a311ea86301787ce3c6e82
SWalls <soerenwalls@gmail.com>
2017-04-20 02:01:53 -0600

    Readded normal data.

--- Projects/Behavioral Cloning/model.py
+++ Projects/Behavioral Cloning/model.py
@@ -29,7 +29,7 @@ def load_samples(data_dir):
                 copy.append("flip")
                 samples.append(copy)
 
-# load_samples('data')
+load_samples('data')
 load_samples('newdata')
 load_samples('reversedata')
 # load_samples('recoverydata')
@@ -100,6 +100,6 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= \
             len(train_samples), validation_data=validation_generator, \
-            nb_val_samples=len(validation_samples), nb_epoch=2)
+            nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
