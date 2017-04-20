87277bc0a93c0c7ad2fc55b88c074b2a3241dcfc
SWalls <soerenwalls@gmail.com>
2017-04-19 23:09:22 -0600

    Added back final dense layer.

--- Projects/Behavioral Cloning/model.py
+++ Projects/Behavioral Cloning/model.py
@@ -87,7 +87,7 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Cropping2D(cropping=((50,20), (0,0))))
 model.add(Convolution2D(24, 5, 5))
 model.add(MaxPooling2D((2, 2)))
 model.add(Activation('relu'))
@@ -104,6 +104,7 @@ model.add(Dense(100))
 model.add(Dropout(0.5))
 model.add(Dense(50))
 model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= \
