bf38b83f7939034d108b7cc426a52e3a9ef19844
SWalls <soerenwalls@gmail.com>
2017-04-19 18:24:20 -0600

    Added behavioral cloning project.

---
+++ Projects/Behavioral Cloning/model.py
@@ -0,0 +1,57 @@
+import csv
+import cv2
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Lambda, Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+lines = []
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for i in range(1, len(lines)):
+    line = lines[i]
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = 'data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+augmented_images, augmented_measurements = [], []
+for image,measurement in zip(images,measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(16, 16, 3))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+model.add(Convolution2D(6, 5, 3))
+model.add(MaxPooling2D((2, 2)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(43))
+model.add(Activation('softmax'))
+model.add(Flatten())
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
