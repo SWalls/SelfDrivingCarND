3bb7ba60120dc8fb4e41fd909708c0a5f6f2d8dc
SWalls <soerenwalls@gmail.com>
2017-04-20 02:46:24 -0600

    Created video from model-a.

--- Projects/Behavioral Cloning/model.py
+++ Projects/Behavioral Cloning/model.py
@@ -104,4 +104,4 @@ model.fit_generator(train_generator, samples_per_epoch= \
             len(train_samples), validation_data=validation_generator, \
             nb_val_samples=len(validation_samples), nb_epoch=3)
 
-model.save('model.h5')
+model.save('model-b.h5')
