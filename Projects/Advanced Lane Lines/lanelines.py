71f26395b2beeecc49975fd52d2fbeb9a7197174
SWalls <soerenwalls@gmail.com>
2017-04-23 21:00:59 -0600

    Added Line class definition to store information about past iterations.

--- Projects/Advanced Lane Lines/lanelines.py
+++ Projects/Advanced Lane Lines/lanelines.py
@@ -4,6 +4,72 @@ import glob
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
+# Number of past iterations to store in a Line.
+n = 10
+# Image shape of all lane lines images.
+img_shape = (1280,720)
+
+# Define a class to receive the characteristics of each line detection
+class Line():
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False
+        # x values of the last n fits of the line
+        self.recent_xfitted = []
+        # average x values of the fitted line over the last n iterations
+        self.bestx = None
+        # polynomial coefficients of the last n fits of the line
+        self.recent_fits = []
+        # polynomial coefficients averaged over the last n iterations
+        self.best_fit = None
+        #radius of curvature of the line in some units
+        self.radius_of_curvature = None
+        # distance in meters of vehicle center from the line
+        self.line_base_pos = None
+        # difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float')
+        # x values for detected line pixels
+        self.allx = None
+        # y values for detected line pixels
+        self.ally = None
+
+    # polynomial coefficients for the most recent fit
+    def current_fit(self):
+        return self.recent_fits[0]
+
+    def add_fit_x_vals(self, fitx):
+        global n
+        if len(self.recent_xfitted) < n:
+            self.recent_xfitted.append(fitx)
+        history_lim = min(n, len(self.recent_xfitted))
+        for i in range(history_lim-1,0,-1):
+            self.recent_xfitted[i] = self.recent_xfitted[i-1]
+        self.recent_xfitted[0] = fitx
+        self.bestx = np.mean(self.recent_xfitted, axis=0)
+
+    def add_fit(self, fit):
+        global n
+        if len(self.recent_fits) < n:
+            self.recent_fits.append(fit)
+        history_lim = min(n, len(self.recent_fits))
+        for i in range(history_lim-1,0,-1):
+            self.recent_fits[i] = self.recent_fits[i-1]
+        self.recent_fits[0] = fit
+        self.best_fit = np.mean(self.recent_fits, axis=0)
+        if len(self.recent_fits) > 1:
+            self.diffs = self.recent_fits[0] - self.recent_fits[1]
+
+    def update(self, fit, fitx, curverad, x, y):
+        global img_shape
+        img_center = img_shape[0]/2.
+        self.detected = True
+        self.add_fit_x_vals(fitx)
+        self.add_fit(fit)
+        self.radius_of_curvature = curverad
+        self.line_base_pos = img_center - x[0]
+        self.allx = x
+        self.ally = y
+
 # Calibrate camera using same image shape for all images.
 def calibrate_camera(img_shape):
     # Chessboard dimensions
@@ -183,7 +249,7 @@ def get_undistorted_binary_lanes(img_filename, cam_mtx, cam_dist, src, dst):
 
     return binary_warped
 
-def find_lanes_sliding_window(binary_warped, img_filename):
+def find_lanes_sliding_window(img_filename, binary_warped, left_line, right_line):
     # Take a histogram of the bottom half of the image
     histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):,:], axis=0)
     # Create an output image to draw on and visualize the result
@@ -243,9 +309,9 @@ def find_lanes_sliding_window(binary_warped, img_filename):
 
     # Extract left and right line pixel positions
     leftx = nonzerox[left_lane_inds]
-    lefty = nonzeroy[left_lane_inds] 
+    lefty = nonzeroy[left_lane_inds]
     rightx = nonzerox[right_lane_inds]
-    righty = nonzeroy[right_lane_inds] 
+    righty = nonzeroy[right_lane_inds]
 
     # Fit a second order polynomial to each
     left_fit = np.polyfit(lefty, leftx, 2)
@@ -266,11 +332,98 @@ def find_lanes_sliding_window(binary_warped, img_filename):
 
     plt.savefig("output_images/%s_polylanes_vis.jpg" % img_filename)
 
-    return left_fit, right_fit
+    # Get radius of curvature in meters.
+    left_curverad, right_curverad = meter_curve_radius(left_fit, right_fit, ploty, left_fitx, right_fitx)
 
-# Image shape of all lane lines images.
-img_shape = (1280,720)
+    # Update lines definitions.
+    left_line.update(left_fit, left_fitx, left_curverad, leftx, lefty)
+    right_line.update(right_fit, right_fitx, right_curverad, rightx, righty)
 
+def find_lanes_limited_search(img_filename, binary_warped, left_fit, right_fit):
+    # Assume you now have a new warped binary image 
+    # from the next frame of video (also called "binary_warped")
+    # It's now much easier to find line pixels!
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin))) 
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))  
+
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    # Create an image to draw on and an image to show the selection window
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    window_img = np.zeros_like(out_img)
+    # Color in left and right line pixels
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    # Visualize: Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    plt.imshow(result)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+
+    plt.savefig("output_images/%s_polylanes_vis_lim.jpg" % img_filename)
+
+    # Get radius of curvature in meters.
+    left_curverad, right_curverad = meter_curve_radius(left_fit, right_fit, ploty, left_fitx, right_fitx)
+
+    # Update lines definitions.
+    left_line.update(left_fit, left_fitx, left_curverad, leftx, lefty)
+    right_line.update(right_fit, right_fitx, right_curverad, rightx, righty)
+
+def pixel_curve_radius(left_fit, right_fit, ploty):
+    # Define y-value where we want radius of curvature
+    # I'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)
+    left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+    right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+    return left_curverad, right_curverad
+
+def meter_curve_radius(left_fit, right_fit, ploty, leftx, rightx):
+    # Define y-value where we want radius of curvature
+    y_eval = np.max(ploty)
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    return left_curverad, right_curverad
+
+img_filename = "straight_lines2"
+
+# Get camera matrix and distortion coefficients.
 cam_mtx, cam_dist = calibrate_camera(img_shape)
 
 # Predefine src and dst points (after distortion) for 
@@ -287,12 +440,14 @@ topp = -3000 # top padding offset
 botp = 0 # bottom padding offset
 undist_dst = np.float32([[xp,topp],[w-xp,topp],[w-xp,h-botp],[xp,h-botp]])
 
-img_filename = "straight_lines2"
-
 # Get binary image of undistorted and unwarped lanes after threhsolding.
 binary_warped = get_undistorted_binary_lanes(img_filename, \
     cam_mtx, cam_dist, undist_src, undist_dst)
 
-# Find the second order polynomial matching each lane.
-left_fit, right_fit = find_lanes_sliding_window(binary_warped, img_filename)
+# Create lines.
+left_line = Line()
+right_line = Line()
+
+# Find the lane lines and populate Line members.
+find_lanes_sliding_window(img_filename, binary_warped, left_line, right_line)
 
