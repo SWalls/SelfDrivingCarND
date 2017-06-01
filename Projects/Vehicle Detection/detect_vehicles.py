a1f949e2728990e23d6497483aa3c962ffafa964
SWalls <soerenwalls@gmail.com>
2017-05-31 22:07:36 -0600

    Almost completed writeup for vehicle detection.

--- Projects/Vehicle Detection/detect_vehicles.py
+++ Projects/Vehicle Detection/detect_vehicles.py
@@ -5,6 +5,7 @@ import cv2
 import glob
 import time
 import os.path
+from random import randint
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
 from skimage.feature import hog
@@ -109,134 +110,6 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
     # Return list of feature vectors
     return features
 
-"""
-# Define a function to extract features from a single image window
-# This function is very similar to extract_features()
-# just for a single image rather than list of images
-def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
-                        hist_bins=32, orient=9, 
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                        spatial_feat=True, hist_feat=True, hog_feat=True):    
-    #1) Define an empty list to receive features
-    img_features = []
-    #2) Apply color conversion if other than 'RGB'
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    else: feature_image = np.copy(img)      
-    #3) Compute spatial features if flag is set
-    if spatial_feat == True:
-        spatial_features = bin_spatial(feature_image, size=spatial_size)
-        #4) Append features to list
-        img_features.append(spatial_features)
-    #5) Compute histogram features if flag is set
-    if hist_feat == True:
-        hist_features = color_hist(feature_image, nbins=hist_bins)
-        #6) Append features to list
-        img_features.append(hist_features)
-    #7) Compute HOG features if flag is set
-    if hog_feat == True:
-        if hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(get_hog_features(feature_image[:,:,channel], 
-                                    orient, pix_per_cell, cell_per_block, 
-                                    vis=False, feature_vec=True))      
-        else:
-            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
-                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        #8) Append features to list
-        img_features.append(hog_features)
-
-    #9) Return concatenated array of features
-    return np.concatenate(img_features)
-
-# Define a function that takes an image,
-# start and stop positions in both x and y, 
-# window size (x and y dimensions),  
-# and overlap fraction (for both x and y)
-def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None], 
-                    xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
-    # If x and/or y start/stop positions not defined, set to image size
-    if x_start_stop[0] == None:
-        x_start_stop[0] = 0
-    if x_start_stop[1] == None:
-        x_start_stop[1] = img.shape[1]
-    if y_start_stop[0] == None:
-        y_start_stop[0] = 0
-    if y_start_stop[1] == None:
-        y_start_stop[1] = img.shape[0]
-    # Compute the span of the region to be searched    
-    xspan = x_start_stop[1] - x_start_stop[0]
-    yspan = y_start_stop[1] - y_start_stop[0]
-    # Compute the number of pixels per step in x/y
-    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
-    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
-    # Compute the number of windows in x/y
-    nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
-    ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
-    nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step) 
-    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step) 
-    # Initialize a list to append window positions to
-    window_list = []
-    # Loop through finding x and y window positions
-    # Note: you could vectorize this step, but in practice
-    # you'll be considering windows one by one with your
-    # classifier, so looping makes sense
-    for ys in range(ny_windows):
-        for xs in range(nx_windows):
-            # Calculate window position
-            startx = xs*nx_pix_per_step + x_start_stop[0]
-            endx = startx + xy_window[0]
-            starty = ys*ny_pix_per_step + y_start_stop[0]
-            endy = starty + xy_window[1]
-            
-            # Append window position to list
-            window_list.append(((startx, starty), (endx, endy)))
-    # Return the list of windows
-    return window_list
-
-# Define a function you will pass an image 
-# and the list of windows to be searched (output of slide_windows())
-def search_windows(img, windows, clf, scaler, color_space='RGB', 
-                    spatial_size=(32, 32), hist_bins=32, 
-                    hist_range=(0, 256), orient=9, 
-                    pix_per_cell=8, cell_per_block=2, 
-                    hog_channel=0, spatial_feat=True, 
-                    hist_feat=True, hog_feat=True):
-
-    #1) Create an empty list to receive positive detection windows
-    on_windows = []
-    #2) Iterate over all windows in the list
-    for window in windows:
-        #3) Extract the test window from original image
-        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))      
-        #4) Extract features for that window using single_img_features()
-        features = single_img_features(test_img, color_space=color_space, 
-                            spatial_size=spatial_size, hist_bins=hist_bins, 
-                            orient=orient, pix_per_cell=pix_per_cell, 
-                            cell_per_block=cell_per_block, 
-                            hog_channel=hog_channel, spatial_feat=spatial_feat, 
-                            hist_feat=hist_feat, hog_feat=hog_feat)
-        #5) Scale extracted features to be fed to classifier
-        test_features = scaler.transform(np.array(features).reshape(1, -1))
-        #6) Predict using your classifier
-        prediction = clf.predict(test_features)
-        #7) If positive (prediction == 1) then save the window
-        if prediction == 1:
-            on_windows.append(window)
-    #8) Return windows for positive detections
-    return on_windows
-"""
-
 def train_model(color_space, spatial_size, hist_bins, orient, pix_per_cell, cell_per_block, hog_channel):
     print("Loading training data.")
 
@@ -340,6 +213,7 @@ def find_cars(img, ystart, ystop, xstart, xstop, scale, svc, X_scaler, color_spa
     hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
     hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
     
+    # Sliding window search!
     bboxes = []
     for xb in range(nxsteps):
         for yb in range(nysteps):
@@ -362,7 +236,7 @@ def find_cars(img, ystart, ystop, xstart, xstop, scale, svc, X_scaler, color_spa
             hist_features = color_hist(subimg, nbins=hist_bins)
 
             # Scale features and make a prediction
-            test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))    
+            test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
             #test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))    
             test_prediction = svc.predict(test_features)
             
@@ -434,32 +308,34 @@ cell_per_block = 2
 hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
 
 svc_model_fname = 'svc_pickel.p'
-if os.path.isfile(svc_model_fname):
+if os.path.isfile(svc_model_fname): # load the model
     dict = joblib.load(svc_model_fname)
-else:
+else: # train the model
     dict = train_model(color_space, spatial_size, hist_bins, orient, pix_per_cell, cell_per_block, hog_channel)
-
 svc = dict["svc"]
 X_scaler = dict["scaler"]
 
 img_width = 1280
 img_height = 720
 
-TEST_PIPELINE = False
+TEST_HOG = False # If True, will test HOG output on test images
+TEST_PIPELINE = False # If True, will test pipeline on test images
 
 if TEST_PIPELINE:
     heat_threshold = 0
 else:
-    heat_threshold = 5
+    heat_threshold = 7
 heat_age = 10 # duration (in frames) of heat to consider
 heat = np.zeros((heat_age,img_height,img_width)).astype(np.float)
 current_heat = 0
 
+# The main pipeline
 def pipeline(img):
     global heat_age, heat, current_heat, heat_threshold
     global svc, X_scaler, color_space, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins
     # Find cars in image.
     bbox_list = []
+    # Gather bounding boxes at 2 different scales, using different search boundaries for each scale.
     for scale, ystart, ystop, xstart, xstop in [[0.8, 400, 445, 430, 930], [1.0, 400, 656, 0, img_width]]:
         bbox_list.extend(find_cars(img, ystart, ystop, xstart, xstop, scale, svc, 
             X_scaler, color_space, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins))
@@ -478,6 +354,7 @@ def pipeline(img):
     heatmap = np.clip(combined_heat, 0, 255)
     # Find final boxes from heatmap using label function
     labels = label(heatmap)
+    # Draw boxes onto the image.
     draw_label_img = draw_labeled_bboxes(img, labels, color=(0, 0, 255), thick=6)
     if TEST_PIPELINE:
         draw_boxes_img = draw_bboxes(img, bbox_list, color=(0, 0, 255), thick=6)
@@ -485,21 +362,34 @@ def pipeline(img):
     else:
         return draw_label_img
 
-if TEST_PIPELINE:
-    for i in range(1, 7):
-        heat = np.zeros((heat_age,img_height,img_width)).astype(np.float)
-        current_heat = 0
-        img = mpimg.imread('test_images/test%d.jpg' % i)
-        draw_label_img, draw_boxes_img, heatmap = pipeline(img)
-        plt.imshow(heatmap, cmap='hot')
-        plt.savefig('output_images/test%d-heatmap.png' % i)
-        plt.imshow(draw_boxes_img)
-        plt.savefig('output_images/test%d-bboxes.png' % i)
-        plt.imshow(draw_label_img)
-        plt.savefig('output_images/test%d-out.png' % i)
+if TEST_HOG:
+    # Save hog output on random image from each class (car, non-car).
+    i = 0
+    for fname in ["training_data/vehicles/KITTI_extracted/%d.png", "training_data/non-vehicles/GTI/image%d.png"]:
+        fnum = randint(1,1000)
+        img = mpimg.imread(fname % fnum)
+        img = img.astype(np.float32)/255
+        f, hog_img = get_hog_features(img[:,:,0], orient, pix_per_cell, cell_per_block, vis=True, feature_vec=True)
+        plt.imshow(hog_img)
+        plt.savefig('output_images/hog-class%d-img%d-%s-orient%d-ppc%d-cpb%d.png' % (i, fnum, color_space, orient, pix_per_cell, cell_per_block))
+        i += 1
 else:
-    # Run pipeline over video.
-    soln_output = 'project_solution.mp4'
-    clip1 = VideoFileClip("project_video.mp4")
-    soln_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
-    soln_clip.write_videofile(soln_output, audio=False)
+    if TEST_PIPELINE:
+        # Save images produced by pipeline using test images as input.
+        for i in range(1, 7):
+            heat = np.zeros((heat_age,img_height,img_width)).astype(np.float)
+            current_heat = 0
+            img = mpimg.imread('test_images/test%d.jpg' % i)
+            draw_label_img, draw_boxes_img, heatmap = pipeline(img)
+            plt.imshow(heatmap, cmap='hot')
+            plt.savefig('output_images/test%d-heatmap.png' % i)
+            plt.imshow(draw_boxes_img)
+            plt.savefig('output_images/test%d-bboxes.png' % i)
+            plt.imshow(draw_label_img)
+            plt.savefig('output_images/test%d-out.png' % i)
+    else:
+        # Run pipeline over video.
+        soln_output = 'project_solution.mp4'
+        clip1 = VideoFileClip("project_video.mp4")
+        soln_clip = clip1.fl_image(pipeline) #NOTE: this function expects color images!!
+        soln_clip.write_videofile(soln_output, audio=False)
---
+++ Projects/Vehicle Detection/writeup_report.md
@@ -0,0 +1,169 @@
+# Vehicle Detection and Tracking
+## By Soeren Walls
+
+---
+
+**Vehicle Detection Project**
+
+The goals / steps of this project are the following:
+
+* Perform a Histogram of Oriented Gradients (HOG) feature extraction on a labeled training set of images and train a classifier Linear SVM classifier
+* Optionally, you can also apply a color transform and append binned color features, as well as histograms of color, to your HOG feature vector. 
+* Note: for those first two steps don't forget to normalize your features and randomize a selection for training and testing.
+* Implement a sliding-window technique and use your trained classifier to search for vehicles in images.
+* Run your pipeline on a video stream (start with the test_video.mp4 and later implement on full project_video.mp4) and create a heat map of recurring detections frame by frame to reject outliers and follow detected vehicles.
+* Estimate a bounding box for vehicles detected.
+
+[//]: # (Image References)
+[image0]: output_images/vehicle-721.png "Vehicle"
+[image1]: output_images/nonvehicle-166.png "Non-Vehicle"
+[image2]: output_images/hog-class0-img842-YCrCb-orient8-ppc8-cpb2.png "HOG example (YCrCb)"
+[image3]: output_images/hog-class0-img721-HSV-orient9-ppc8-cpb2.png "HOG example (HSV)"
+[image4]: output_images/test1-bboxes.png "Bounding Boxes"
+[image5]: output_images/test1-heatmap.png "Heatmap"
+[image6]: output_images/test1-out.png "Output"
+[image7]: output_images/video_frames/frame-35.png "Frame 35"
+[image8]: output_images/video_frames/frame-36.png "Frame 36"
+[image9]: output_images/video_frames/frame-37.png "Frame 37"
+[image10]: output_images/video_frames/frame-38.png "Frame 38"
+[image11]: output_images/video_frames/frame-39.png "Frame 39"
+[image12]: output_images/video_frames/frame-40.png "Frame 40"
+[image13]: output_images/video_frames/frame-41.png "Frame 41"
+[image14]: output_images/video_frames/frame-42.png "Frame 42"
+[image15]: output_images/video_frames/frame-43.png "Frame 43"
+[image16]: output_images/video_frames/frame-44.png "Frame 44"
+[image17]: output_images/video_frames/labels-44.png "Labels"
+[image18]: output_images/video_frames/bboxes-44.png "BBoxes"
+
+## [Rubric](https://review.udacity.com/#!/rubrics/513/view) Points
+Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
+
+---
+### Writeup / README
+
+1\. **Provide a Writeup / README that includes all the rubric points and how you addressed each one. You can submit your writeup as markdown or pdf. [Here](https://github.com/udacity/CarND-Vehicle-Detection/blob/master/writeup_template.md) is a template writeup for this project you can use as a guide and a starting point.**
+
+You're reading it!
+
+### Histogram of Oriented Gradients (HOG)
+
+1\. **Explain how (and identify where in your code) you extracted HOG features from the training images.**
+
+The code for this step is contained in lines 73 through 111 of the file `detect_vehicles.py`. This file contains all of my code for this project.
+
+I started by reading in all the `vehicle` and `non-vehicle` images (lines 116-123). There are roughly 4,000 images in each class. It's important that both classes have approximately the same number of training data points, because this helps avoid unwanted class bias during training. Here is an example of one of each of the `vehicle` and `non-vehicle` classes:
+
+![alt text][image0]
+![alt text][image1]
+
+I then explored different color spaces and different `skimage.hog()` parameters (`orientations`, `pixels_per_cell`, and `cells_per_block`). I grabbed random images from each of the two classes and displayed them to get a feel for what the `skimage.hog()` output looks like.
+
+Here is an example using the `YCrCb` color space and HOG parameters of `orientations=8`, `pixels_per_cell=(8, 8)` and `cells_per_block=(2, 2)` on an image from the `vehicle` data set:
+
+![alt text][image2]
+
+2\. **Explain how you settled on your final choice of HOG parameters.**
+
+I tried various combinations of parameters and decided on the following:
+
+```
+color_space = 'HSV'
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+```
+
+This is because these parameters seem to produce a more consistently sensical output for HOG as compared to others. Here is an example HOG output using these parameters on an image from the `vehicle` data set:
+
+![alt text][image3]
+
+As you can see, whereas the image produced using the `YCrCb` color space does not have a clear outline, this image clearly describes the outline of a car. This intuition about how it looks led me to choose these parameters for the classifier, and using these parameters does indeed improve the accuracy.
+
+3\. **Describe how (and identify where in your code) you trained a classifier using your selected HOG features (and color features if you used them).**
+
+I trained a linear SVM (at lines 113-181 in my code) using features from HOG, spatial binning, and color histograms, with the following parameters, hard-coded at lines 302-308:
+
+```
+color_space = 'HSV'
+spatial_size = (32, 32)
+hist_bins = 16
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+hog_channel = "ALL"
+```
+
+The features from all 3 of these operations are combined into a single feature vector, which is labeled with either a 1 or a 0, depending on whether it belongs to the `vehicle` or `non-vehicle` class, respectively.
+
+### Sliding Window Search
+
+1\. **Describe how (and identify where in your code) you implemented a sliding window search. How did you decide what scales to search and how much to overlap windows?**
+
+Instead of using the sliding window search function described in the earlier lessons, which computes the HOG features over and over for each window, I decided to use the `find_cars` function, defined at lines 183-250, which computes the HOG features beforehand, and then performs the sliding window search while re-using the same feature data. This proved much more efficient and saved a lot of time.
+
+In the `pipeline` function, `find_cars` is called twice at two different scales: 0.8 and 1.0. This can be seen at lines 338-350. However, different search boundaries are defined for each scale. Specifically, the algorithm searches with windows at 0.8 scale only in the top 45 pixels of the road and between the middle 500 pixels, since this is where cars are expected to appear small. It searches with windows at 1.0 scale only in the lower 500 pixels of the road. This can be seen on line 339:
+
+```
+for scale, ystart, ystop, xstart, xstop in [[0.8, 400, 445, 430, 930], [1.0, 400, 656, 0, img_width]]:
+```
+
+These scales were chosen because a scale that is too small (such as 0.5) results in a lot of false-positives, and all scales bigger than 1.0 are redundant, as they seem to identify cars equally as well as the 1.0 scale. As such, these extra scales aren't worth the performance loss to compute.
+
+Instead of overlap, the `find_cars` function defines at line 207 how many cells to jump per step of the sliding window algorithm. I chose to keep the default value of 2 cells per step, since this provides some overlap without being too performance heavy.
+
+2\. **Show some examples of test images to demonstrate how your pipeline is working. What did you do to optimize the performance of your classifier?**
+
+Ultimately I searched on two scales using HSV 3-channel HOG features plus spatially binned color and histograms of color in the feature vector, which provided a nice result. To otpimize performance, I combined the hog feature extraction and sliding window search into one function.
+
+First, the pipeline calls the `find_cars` function to get all the bounding boxes of suspected vehicles in the image at 2 different scales. Here is an example of the bounding boxes produced by this function:
+
+![alt text][image4]
+
+Next, the pipeline uses the bounding boxes to create a heatmap that shows the most likely candidates for separate cars in the image. This can be seen at lines 350-354. This is done over multiple frames, and then the heatmap is thresholded, so that false positives that only appear in a few frames are removed. Here is an example of a thresholded heatmap, created from the bounding boxes in the previous image:
+
+![alt text][image5]
+
+Finally, the pipeline uses the `labels` function to identify separate cars in the image. These labels are then drawn over the cars to produce the final output:
+
+![alt text][image6]
+
+---
+
+### Video Implementation
+
+1. **Provide a link to your final video output.  Your pipeline should perform reasonably well on the entire project video (somewhat wobbly or unstable bounding boxes are ok as long as you are identifying the vehicles most of the time with minimal false positives.)**
+
+Here's a [link to my video result](./project_video.mp4)
+
+2. **Describe how (and identify where in your code) you implemented some kind of filter for false positives and some method for combining overlapping bounding boxes.**
+
+I record the positions of positive detections for 10 frames at a time, and store them in an array of heatmaps called `heat` (at line 330). In each frame, I append the current frame's heatmap to `heat` before calculating the sum of all 10 heatmaps in `heat`, to get one single heatmap representing the past 10 frames. I then threshold that map by 7 to remove false positives and identify vehicle positions. This is based on the intuition that a detection is a false positive if it is a bounding box overlapped by 7 or fewer other bounding boxes from the same frame or a combination of frames from the previous 10 frames. I then use `scipy.ndimage.measurements.label()` to identify individual blobs in the heatmap. I then assume each blob corresponds to a separate vehicle. Finally, I construct bounding boxes to cover the area of each blob that has been detected.
+
+Here's an example result showing the heatmap from a series of frames of video, the result of `scipy.ndimage.measurements.label()` and the bounding boxes then overlaid on the last frame of video:
+
+#### Here are 10 frames and their corresponding heatmaps:
+![alt text][image7]
+![alt text][image8]
+![alt text][image9]
+![alt text][image10]
+![alt text][image11]
+![alt text][image12]
+![alt text][image13]
+![alt text][image14]
+![alt text][image15]
+![alt text][image16]
+
+#### Here is the output of `scipy.ndimage.measurements.label()` on the integrated heatmap from all 10 frames:
+![alt text][image17]
+
+#### Here the resulting bounding boxes are drawn onto the last frame in the series:
+![alt text][image18]
+
+---
+
+### Discussion
+
+1\. **Briefly discuss any problems / issues you faced in your implementation of this project. Where will your pipeline likely fail? What could you do to make it more robust?**
+
+Here I'll talk about the approach I took, what techniques I used, what worked and why, where the pipeline might fail and how I might improve it if I were going to pursue this project further.  
+
