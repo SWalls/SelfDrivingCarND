b2161945e9c9995ca3b259d152f07a756e4befca
SWalls <soerenwalls@gmail.com>
2017-09-15 16:50:04 -0700

    Updated noise parameters to improve RMSE to acceptable levels.

--- Term 2/Projects/Unscented Kalman Filter/src/ukf.cpp
+++ Term 2/Projects/Unscented Kalman Filter/src/ukf.cpp
@@ -51,10 +51,10 @@ UKF::UKF()
     previous_timestamp_ = 0;
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 0.2;
+    std_a_ = 1;
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 0.2;
+    std_yawdd_ = 0.5;
 
     // Laser measurement noise standard deviation position1 in m
     std_laspx_ = 0.15;
