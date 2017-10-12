46fad8439f0cd6b03039480efa65928e81a51083
Soeren Walls <soerenwalls@hotmail.com>
2017-10-11 21:17:51 -0700

    Coded most of basic solution for MPC.

--- Term 2/Projects/MPC/src/main.cpp
+++ Term 2/Projects/MPC/src/main.cpp
@@ -17,6 +17,8 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
+const double Lf = 2.67;
+
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
