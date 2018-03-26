62a3bc647354ed6ec40236e991ef1d93b9e075bf
SWalls <soerenwalls@gmail.com>
2018-03-25 18:53:07 -0700

    Implement spline interpolation for smoother path generation.

--- Term 3/Projects/Path Planning/src/main.cpp
+++ Term 3/Projects/Path Planning/src/main.cpp
@@ -164,6 +164,143 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
+double convertMphToMps(double mph) {
+	return mph/2.24; // rough conversion
+}
+
+struct SplineYaw {
+	tk::spline s;
+	double ref_yaw;
+};
+
+// Creates a spline that interpolates a set of generated waypoints 30, 60, and 90
+// meters ahead of the car's current location.
+SplineYaw createSpline(const vector<double> &previous_path_x, const vector<double> &previous_path_y, double car_x, double car_y, double car_yaw, double car_s, int current_lane, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y) {
+	int prev_path_size = previous_path_x.size();
+
+	// First, create a list of widely spaced (x,y) waypoints, evenly spaced at 30m.
+	// Later, we'll interpolate these waypoints with a spline and fill it with more
+	// points at specified intervals to give the car the desired speed.
+	vector<double> waypoints_x;
+	vector<double> waypoints_y;
+
+	// We will build these waypoints starting from reference x,y,yaw states. We will
+	// either reference the car's current state, or the end state of the previous path.
+	double ref_x;
+	double ref_y;
+	double ref_yaw;
+	double prev_ref_x;
+	double prev_ref_y;
+
+	if (prev_path_size < 2) {
+		// If previous path size is not big enough, use the car's current state.
+		ref_x = car_x;
+		ref_y = car_y;
+		ref_yaw = deg2rad(car_yaw);
+		prev_ref_x = car_x - cos(car_yaw);
+		prev_ref_y = car_y - sin(car_yaw);
+	} else {
+		// Otherwise, use the previous path's end point as starting reference.
+		ref_x = previous_path_x[prev_path_size-1];
+		ref_y = previous_path_y[prev_path_size-1];
+		prev_ref_x = previous_path_x[prev_path_size-2];
+		prev_ref_y = previous_path_y[prev_path_size-2];
+		ref_yaw = atan2(ref_y - prev_ref_y, ref_x - prev_ref_x);
+	}
+
+	waypoints_x.push_back(prev_ref_x);
+	waypoints_x.push_back(ref_x);
+	waypoints_y.push_back(prev_ref_y);
+	waypoints_y.push_back(ref_y);
+
+	// In Frenet coordinates, add evenly spaced (30m apart) waypoints ahead of the car.
+	double next_d = 2 + 4 * current_lane;
+	vector<double> waypoint_0 = getXY(car_s+30, next_d, maps_s, maps_x, maps_y);
+	vector<double> waypoint_1 = getXY(car_s+60, next_d, maps_s, maps_x, maps_y);
+	vector<double> waypoint_2 = getXY(car_s+90, next_d, maps_s, maps_x, maps_y);
+
+	waypoints_x.push_back(waypoint_0[0]);
+	waypoints_x.push_back(waypoint_1[0]);
+	waypoints_x.push_back(waypoint_2[0]);
+	waypoints_y.push_back(waypoint_0[1]);
+	waypoints_y.push_back(waypoint_1[1]);
+	waypoints_y.push_back(waypoint_2[1]);
+
+	// Shift the waypoints reference angle to 0 degrees relative to the car,
+	// to make the math easier later.
+	for (int i = 0; i < waypoints_x.size(); i++) {
+		double shift_x = waypoints_x[i] - ref_x;
+		double shift_y = waypoints_y[i] - ref_y;
+		waypoints_x[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+		waypoints_y[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+	}
+
+	// Create a spline to interpolate the waypoints.
+	tk::spline s;
+	s.set_points(waypoints_x, waypoints_y);
+
+	SplineYaw splineYaw;
+	splineYaw.s = s;
+	splineYaw.ref_yaw = ref_yaw;
+	return splineYaw;
+}
+
+struct PointVals {
+	vector<double> x_vals;
+	vector<double> y_vals;
+};
+
+// Generates the actual (x,y) points we will use for the path planner.
+PointVals getXYValsFromSpline(const vector<double> &previous_path_x, const vector<double> &previous_path_y, int path_size, tk::spline s, double ref_yaw, double ref_velocity) {
+	int prev_path_size = previous_path_x.size();
+
+	vector<double> next_x_vals;
+	vector<double> next_y_vals;
+
+	// Start the path with all the points leftover from the previous path.
+	for (int i = 0; i < prev_path_size; i++) {
+		next_x_vals.push_back(previous_path_x[i]);
+		next_y_vals.push_back(previous_path_y[i]);
+	}
+
+	// Calculate how to break up the spline points so the car travels at the
+	// desired reference velocity. We do this by predicting a horizon 30m into
+	// the future, and linearly seperating the spline into sections. The length
+	// of each section is determined by the reference velocity.
+	double horizon_x = 30.0; // meters
+	double horizon_y = s(horizon_x);
+	double horizon_dist = sqrt(horizon_x * horizon_x + horizon_y * horizon_y);
+	double time_inc = .02; // seconds
+	double N_sections = horizon_dist / (time_inc * convertMphToMps(ref_velocity));
+	double section_start_x = 0;
+
+	// Fill up the rest of the path!
+	for (int i = 0; i < path_size - prev_path_size; i++) {
+		double section_x = section_start_x + (horizon_x / N_sections);
+		double section_y = s(section_x);
+		section_start_x = section_x;
+
+		double shift_x = section_x;
+		double shift_y = section_y;
+
+		// Rotate the section point back to the reference orientation to undo
+		// the earlier rotation.
+		section_x = shift_x * cos(ref_yaw) - shift_y * sin(ref_yaw);
+		section_y = shift_x * sin(ref_yaw) + shift_y * cos(ref_yaw);
+
+		section_x += shift_x;
+		section_y += shift_y;
+
+		next_x_vals.push_back(section_x);
+		next_y_vals.push_back(section_y);
+	}
+
+	PointVals pointVals;
+	pointVals.x_vals = next_x_vals;
+	pointVals.y_vals = next_y_vals;
+	return pointVals;
+}
+
 int main() {
   uWS::Hub h;
 
@@ -201,7 +338,16 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+	// Start in lane 1
+	int current_lane = 1;
+
+	// The car's motion will target this velocity.
+	double ref_velocity = 49.5; // mph
+
+	// The size of the path we want to generate.
+	int path_size = 50;
+
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&current_lane,&ref_velocity,&path_size](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -238,20 +384,21 @@ int main() {
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
+						// Generate a spline that interpolates a set of waypoints, as well as the
+						// reference yaw of the car.
+						SplineYaw splineYaw = createSpline(previous_path_x, previous_path_y, car_x, car_y, car_yaw, car_s, current_lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
           	json msgJson;
 
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
-
-          	// Define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-						double dist_inc = 0.5;
-						for (int i = 0; i < 50; i++) {
-							double next_s = car_s+(i+1)*dist_inc;
-							double next_d = 6;
-							vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-							next_x_vals.push_back(xy[0]);
-							next_y_vals.push_back(xy[1]);
-						}
+						// Define the actual (x,y) points we will use for the path planner.
+						PointVals pointVals = getXYValsFromSpline(previous_path_x, previous_path_y, path_size, splineYaw.s, splineYaw.ref_yaw, ref_velocity);
+          	vector<double> next_x_vals = pointVals.x_vals;
+          	vector<double> next_y_vals = pointVals.y_vals;
+						
+						cout << "y-vals: ";
+						for (vector<double>::const_iterator i = next_y_vals.begin(); i != next_y_vals.end(); ++i)
+								cout << *i << ' ';
+						cout << '\n';
 
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
